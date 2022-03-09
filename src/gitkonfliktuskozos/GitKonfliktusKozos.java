eb113c9e7d6cf2021036b2b1e620a90580c4267c
Richard-0106 <alex19950106@gmail.com>
2022-03-09 12:21:06 +0100

    1.név kitöltése

--- src/gitkonfliktuskozos/GitKonfliktusKozos.java
+++ src/gitkonfliktuskozos/GitKonfliktusKozos.java
@@ -3,7 +3,9 @@ package gitkonfliktuskozos;
 public class GitKonfliktusKozos {
 
     public static void main(String[] args) {
-        String nev1,nev2;
+        String nev1="Ricsi";
+        String nev2;
+        String nev3;
     }
     
 }
