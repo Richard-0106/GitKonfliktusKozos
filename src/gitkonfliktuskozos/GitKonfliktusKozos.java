9e70fe692a332ae5fc24b89aacd8983b544d7b3a
SchmiedmeiszterAdam <smikuadam@gmail.com>
2022-03-09 12:21:46 +0100

    2.név inicializálása

--- src/gitkonfliktuskozos/GitKonfliktusKozos.java
+++ src/gitkonfliktuskozos/GitKonfliktusKozos.java
@@ -3,7 +3,9 @@ package gitkonfliktuskozos;
 public class GitKonfliktusKozos {
 
     public static void main(String[] args) {
-        String nev1,nev2;
+        String nev1;
+        String nev2 = "Ádám";
+        String nev3;
     }
     
 }
